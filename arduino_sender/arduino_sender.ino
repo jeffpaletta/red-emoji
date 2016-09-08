15006b61ed21be8a0666f1f1bb2ddd76003ba4ea
Jeff Paletta <jeffpaletta@gmail.com>
2016-09-08 13:28:58 -0400

    Revert "Tracking lfs changes"
    This reverts commit 522f99e8bd921fe89261c273788606ffa9c8a845.

--- arduino_sender/arduino_sender.ino
+++ arduino_sender/arduino_sender.ino
@@ -1,7 +1,7 @@
 #include <Servo.h>
 Servo myservo;  // create servo object to control a servo
 
-
+char val;
 
 int switchState = 0;      // twelve servo objects can be created on most boards
 int switchState7 = 0;
@@ -26,43 +26,59 @@ void loop() {
   switchState7 = digitalRead(7);
 
   if (switchState == LOW) {
-    
     Serial.println("2_LOW");
-    for (pos = 0; pos <= 0; pos += 1) // goes from 0 degrees to 180 degrees
-      { // in steps of 1 degree
-        myservo.write(pos);              // tell servo to go to position in variable 'pos'
-        delay(5);
-      }
-    
   } else if (switchState == HIGH) {
-    
     Serial.println("2_HIGH");
-     for (pos = 60; pos >= 0; pos -= 1) // goes from 180 degrees to 0 degrees
-    {
-      myservo.write(pos);              // tell servo to go to position in variable 'pos'
-      delay(5);                       // waits 15ms for the servo to reach the position
-    } 
     // delay(5000);
-    
   }
 
   if (switchState7 == LOW) {
     Serial.println("7_LOW");
-    for (pos = 0; pos <= 0; pos += 1) // goes from 0 degrees to 180 degrees
-      { // in steps of 1 degree
-        myservo.write(pos);              // tell servo to go to position in variable 'pos'
-        delay(5);
-      }
   } else if (switchState7 == HIGH) {
     Serial.println("7_HIGH");
-     for (pos = 60; pos >= 0; pos -= 1) // goes from 180 degrees to 0 degrees
+    //delay(5000);
+  }
+
+  if (Serial.available())
+  { // If data is available to read,
+    val = Serial.read(); // read it and store it in val
+
+  }
+}
+ /* if (val == '1')
+  { // If 1 was received
+    for (pos = 60; pos >= 0; pos -= 1) // goes from 180 degrees to 0 degrees
     {
       myservo.write(pos);              // tell servo to go to position in variable 'pos'
       delay(5);                       // waits 15ms for the servo to reach the position
     } 
-    //delay(5000);
   }
+  else {
+  }
+  if (val == '2')
+  { // If 1 was received
+    for (pos = 60; pos >= 0; pos -= 1) // goes from 180 degrees to 0 degrees
+    {
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(5);                       // waits 15ms for the servo to reach the position
+    } 
+  }
+  else {
+  }
+  }*/
+  /*else {
+      for (pos = 0; pos <= 0; pos += 1) // goes from 0 degrees to 180 degrees
+      { // in steps of 1 degree
+        myservo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(5);                       // waits 15ms for the servo to reach the position
+      }
+    }
+*/
 
- 
-}
+    /*digitalWrite(ledPin, HIGH); // turn the LED on
+      } else {
+      digitalWrite(ledPin, LOW); // otherwise turn it off
+      }
+      delay(10); // Wait 10 milliseconds for next reading*/
+  
 
